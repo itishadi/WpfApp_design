7d3762965c7066e597141fcdfb192c2416efb614
itishadi <113441103+itishadi@users.noreply.github.com>
2023-09-15 15:17:32 +0200

    Add project files.

---
+++ ServiceApplication/MVVM/Models/DeviceItem.cs
@@ -0,0 +1,41 @@
+﻿namespace ServiceApplication.MVVM.Models;
+
+public class DeviceItem
+{
+    public DeviceItem() { }
+    
+    public string? DeviceId { get; set; }
+    public string? DeviceType { get; set; }
+    public string? Placement { get; set; }
+    public bool IsActive { get; set; } = false;
+    public string? Icon => SetIcon();
+    
+
+    private string SetIcon()
+    {
+        var icon = "\uf2db";
+        switch (DeviceType?.ToLower())
+        {
+            case "light":
+                icon = "\uf0eb";
+                break;
+
+            case "lamp":
+                icon = "\ue004";
+                break;
+
+            default:
+                icon = "\uf2db";
+                break;
+
+        }
+
+        //return (DeviceType?.ToLower()) switch
+        //{
+        //    "light" => "\uf0eb",
+        //    "lamp" => "\ue004",
+        //    "fan" => "\uf2db"
+        //};
+        return icon;
+    }
+}
---
+++ ServiceApplication/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using ServiceApplication.MVVM.Models;
+
+namespace ServiceApplication
+{
+ 
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<DeviceItem> Devices { get; set; } = new ObservableCollection<DeviceItem>()
+        {
+            new DeviceItem {DeviceId = "1",DeviceType="light", Placement = "Kicken", IsActive = true},
+             new DeviceItem {DeviceId = "2",DeviceType="ledstrip" ,Placement = "Livingroom", IsActive = false},
+              new DeviceItem {DeviceId = "3",DeviceType="fan", Placement = "Toa", IsActive = false}
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            DeviceList.ItemsSource = Devices;
+
+            //this.WindowState = WindowState.Maximized;
+            //this.WindowStartupLocation = WindowStartupLocation.Manual;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+
+        }
+    }
+}
